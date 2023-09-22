1e85bac3bb3d18134ed908f5ce04ba01528b45dd
Kishor B K <kishorbkgowda36@gmail.com>
2023-09-22 14:49:05 +0530

    final changes done
    yeah added favicon , used objects creation  ...

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+let playerobj = 
+ {
+     name : "SPICY",
+     chips : 200
+}
+
 let p1 = document.querySelector('.p1');
 let p2 = document.querySelector('.p2');
 let p3 = document.querySelector('.p3');
+let p4 = document.querySelector('.p4');
 let btns  = document.querySelectorAll('button');
 
 let msg = "";
-
 let firststart = false;
 let hasblackjack = false;
-let isalive = true;
+let isalive = false;
+
+
 let card1 = getrandomnumber();
 let card2 = getrandomnumber();
-let arr = [card1,card2];
-let sum = card1 + card2;
-
 
 function getrandomnumber()
 {
@@ -20,56 +25,11 @@ function getrandomnumber()
     return random;
 }
 
-let rendergame=()=>
-{
-            if(sum <= 20)
-                {
-                    msg = "Do you want to draw a new card?";
-                }
 
-            else if(sum === 21)
-                {
-                    msg = "You've got Blackjack!";
-                    hasblackjack = true;
-                }
+let arr = [card1,card2];
+let sum = card1 + card2;
 
-            else
-                {
-                    msg = "You're out of the game!";
-                    isalive = false;
-                }
-            p1.textContent = msg;
-            p2.textContent = `${finalshow()}`;
-            p3.textContent = `Sum : ${sum}`;
-
-}
-
-
-let startgame=()=>
-{
-    firststart = false;
-    isalive = true;
-    card1 = getrandomnumber();
-    card2 = getrandomnumber();
-    arr = [card1,card2];
-    sum = card1 + card2;
-
-    rendergame();
-}
-
-
-let finalshow=()=>
-{
-    let cardno = "";
-    for(let i=0 ; i<arr.length;i++)
-    {
-         cardno = cardno + " " +arr[i];
-         console.log(cardno);
-    }
-     return p2.textContent = `Cards : ${cardno}`;
-
-    
-}
+p4.textContent = playerobj.name +" : "+playerobj.chips+"$";
 
 btns.forEach(btn => 
     {
@@ -78,14 +38,13 @@ btns.forEach(btn =>
            if(e.target.innerText == 'START GAME')
            {
               startgame();
-              firststart = true;
            }
             
 
            
-                if(e.target.innerText == 'NEW CARD' && firststart == true)
+                if(e.target.innerText == 'NEW CARD' )
                 {
-                    if(isalive == true && hasblackjack === false)
+                    if(isalive === true && hasblackjack === false)
                         {
                             let card3 = getrandomnumber();
                             arr.push(card3);
@@ -106,3 +65,65 @@ btns.forEach(btn =>
 });
 
 })
+
+
+let startgame=()=>
+{
+    firststart = false;
+    isalive = true;
+    hasblackjack = false;
+    card1 = getrandomnumber();
+    card2 = getrandomnumber();
+    arr = [card1,card2];
+    sum = card1 + card2;
+
+    rendergame();
+}
+
+
+
+
+let rendergame=()=>
+{
+            if(sum <= 20)
+                {
+                    msg = "Do you want to draw a new card?";
+                }
+
+            else if(sum === 21)
+                {
+                    msg = "You've got Blackjack!";
+                    hasblackjack = true;
+                    playerobj.chips = playerobj.chips + 100;
+                    p4.textContent = playerobj.name +" : "+playerobj.chips+"$";
+                }
+
+            else
+                {
+                    msg = "You're out of the game!";
+                    isalive = false;
+                    playerobj.chips = playerobj.chips - 100;
+                    p4.textContent = playerobj.name +" : "+playerobj.chips+"$";
+
+                }
+            p1.textContent = msg;
+            p2.textContent = `${finalshow()}`;
+            p3.textContent = `Sum : ${sum}`;
+            
+          
+}
+
+
+
+let finalshow=()=>
+{
+    let cardno = "";
+    for(let i=0 ; i<arr.length;i++)
+    {
+         cardno = cardno + " " +arr[i];
+         console.log(cardno);
+    }
+     return p2.textContent = `Cards : ${cardno}`;
+
+    
+}
